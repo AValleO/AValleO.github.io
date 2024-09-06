88adccc39605283adc1429dca2a9ec26471377c2
AValleO <145483052+AValleO@users.noreply.github.com>
2024-09-06 04:12:19 -0500

    Delete capacitor.config.ts

--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.pronaca.compressor',
-  appName: 'compressor-app',
-  webDir: 'dist'
-};
-
-export default config;
